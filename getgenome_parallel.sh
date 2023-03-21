dfd0576aaf6e6cd3844c0e130c3335367158ce75
Liang Xu <xl0418@gmail.com>
2023-03-21 09:28:19 -0700

    parallel added

--- getgenome_parallel.sh
+++ getgenome_parallel.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+dl_data(){
+    info=($1)
+    str1=${info[0]} # blast id, e.g. c497da3b39f30aceede6bec3b03cd100
+    str2=${info[1]} # genome id, e.g. GB_GCA_905618805.1
+    echo $str1 
+    echo $str2
+
+    # create folders to hold each species and further down to matched genomes for each species
+    mkdir -p $str1/$str2
+    # get database and the correct genome id, e.g. sourceid=GB, genomeid=GCA_905618805.1
+    IFS='_' read -ra ADDR <<< "$str2"
+    sourceid="${ADDR[0]}"
+    genomeid="${ADDR[1]}_${ADDR[2]}" 
+    
+
+    # downloaed zip file and file directory
+    output="${genomeid}.zip"
+    filedir="${str1}/${str2}/${output}"    
+    
+    echo $output
+    echo $filedir
+    # download data using datasets from ncbi
+    ./datasets download genome accession $genomeid --filename $filedir
+
+    unzip $filedir -d "${str1}/${str2}"
+}
+
+
 echo "FileReading Starts"
 
 # Get the total number of lines in the file
@@ -13,27 +41,7 @@ line_number=1
 while IFS= read -r line
 do
     # extract the blast species id and the genome matching id 
-    info=($line)
-    str1=${info[0]} # blast id, e.g. c497da3b39f30aceede6bec3b03cd100
-    str2=${info[1]} # genome id, e.g. GB_GCA_905618805.1
-
-    # create folders to hold each species and further down to matched genomes for each species
-    mkdir -p $str1/$str2
-    # get database and the correct genome id, e.g. sourceid=GB, genomeid=GCA_905618805.1
-    IFS='_' read -ra ADDR <<< "$str2"
-    sourceid="${ADDR[0]}"
-    genomeid="${ADDR[1]}_${ADDR[2]}" 
-    
-    # downloaed zip file and file directory
-    output="${genomeid}.zip"
-    filedir="${str1}/${str2}/${output}"    
-    
-    echo $output
-    echo $filedir
-    # download data using datasets from ncbi
-    ./datasets download genome accession $genomeid --filename $filedir
-
-    unzip $filedir -d "${str1}/${str2}"
+    dl_data "$line"
     
     # Print the progress bar
     echo -ne "\rProcessing line $line_number of $total_lines"
