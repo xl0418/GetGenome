c91998d1f9aed5ef408eb45849e5d42d0e009bed
Liang Xu <xl0418@gmail.com>
2023-04-21 09:33:23 -0700

    updated

--- sub_prokkagenome_P16NS.bash
+++ sub_prokkagenome_P16NS.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 ## now loop through the above array
 
-#SBATCH --time=6-20:10:00   # walltime
+#SBATCH --time=6-23:10:00   # walltime
 #SBATCH --ntasks=32   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=10G   # memory per CPU core
-#SBATCH -J "ProkkaSPOT"   # job name
+#SBATCH -J "ProkkaP16NS"   # job name
 #SBATCH --mail-user=liangxu@caltech.edu   # email address
 
 module load parallel/20180222
 module load singularity/3.3.0
 
-sh prokka_genomes_SPOT.sh 32 SPOT_Prokaryotic16S_ASV_dna-sequences_BLASToutput.tsv
+sh prokka_genomes_P16NS.sh 32 221117-1910.P16N-S.16S.dna-sequences.tsv
 
 
