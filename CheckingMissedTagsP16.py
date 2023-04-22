83cb1951da646d18facce39b37a20a5d334e29cb
Liang Xu <xl0418@gmail.com>
2023-04-21 21:12:46 -0700

    bug fixed

--- CheckingMissedTagsP16.py
+++ CheckingMissedTagsP16.py
@@ -14,9 +14,10 @@ def p16_missed_species(i):
         missed_species =pd.DataFrame({'Species': ori_data.iloc[i,0], 'Kingdom': missed_kingdom}, index=[0])
         return missed_species
 
-p16_blast = pd.read_csv("P16NSdata/221117-1910.P16N-S.16S.dna-sequences.tsv", sep='\t', header=None)
+
+ori_data = pd.read_csv("P16NSdata/221118-0208.P16N-S.16S.all-16S-seqs.with-tax.proportions.tsv", sep='\t')
 
 with mp.Pool(mp.cpu_count() - 1) as pool:
-    data = pool.map(p16_missed_species, [i for i in range(1200)])
+    data = pool.map(p16_missed_species, [i for i in range(ori_data.shape[0])])
 p16missed = pd.concat(data, axis=0)
 p16missed.to_csv('P16missedTags.csv')
--- CheckingMissedTagsSPOT.py
+++ CheckingMissedTagsSPOT.py
@@ -3,20 +3,20 @@ import multiprocess as mp
 
 def spot_missed_species(i):
     # find species that have no hits in BLAST for SPOT dataset
-    spot_blast = pd.read_csv("SPOT/SPOT_Prokaryotic16S_ASV_Domain_identity.csv", sep='\t', header=None)
-    spot_data = pd.read_csv("SPOT/SPOT_Prokaryotic16S_ASV_dna-sequences_BLASToutput.tsv", sep='\t')
-    speciestag = spot_blast.iloc[i, 0].split(",")[0]
+    spot_data = pd.read_csv("SPOTdata/SPOT_Prokaryotic16S_ASV_Domain_identity.csv", sep='\t')
+    spot_blast = pd.read_csv("SPOTdata/SPOT_Prokaryotic16S_ASV_dna-sequences_BLASToutput.tsv", sep='\t', header=None)
+    speciestag = spot_data.iloc[i, 0].split(",")[0]
     speciestag = speciestag.replace(">", "")
 
-    if speciestag not in spot_data.iloc[:,0].unique():
-        missed_kingdom = spot_blast.iloc[i, 0].split(",")[1]
-        missed_species_spot =pd.DataFrame({'Species': spot_blast.iloc[i,0], 'Kingdom': missed_kingdom}, index=[0])
+    if speciestag not in spot_blast.iloc[:,0].unique():
+        missed_kingdom = spot_data.iloc[i, 0].split(",")[1]
+        missed_species_spot =pd.DataFrame({'Species': speciestag, 'Kingdom': missed_kingdom}, index=[0])
         return missed_species_spot
 
-spot_blast = pd.read_csv("SPOT/SPOT_Prokaryotic16S_ASV_Domain_identity.csv", sep='\t', header=None)
+spot_data = pd.read_csv("SPOTdata/SPOT_Prokaryotic16S_ASV_Domain_identity.csv", sep='\t')
 
 
 with mp.Pool(mp.cpu_count() - 1) as pool:
-    data = pool.map(spot_missed_species, [i for i in range(spot_blast.shape[0])])
+    data = pool.map(spot_missed_species, [i for i in range(spot_data.shape[0])])
 spotmissed = pd.concat(data, axis=0)
 spotmissed.to_csv('spotmissedTags.csv')
